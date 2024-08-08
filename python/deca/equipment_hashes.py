5aa9b6cb1f8855f495bd7caafcafaca0ab416c23
Ryan Maxim <ryan.a.maxim@gmail.com>
2024-08-08 12:00:59 -0400

    animal interest equipment lookup

---
+++ python/deca/equipment_hashes.py
@@ -0,0 +1,27 @@
+from deca.ff_rtpc import rtpc_from_binary, FieldNameMap
+from deca.db_core import VfsDatabase
+from pathlib import Path
+import logging
+import json
+
+def open_file(filename: Path):
+ with(filename.open("rb")) as f:
+   data = rtpc_from_binary(f)
+ f_bytes = bytearray(filename.read_bytes())
+ return (data.root_node, f_bytes)
+
+DECA_PATH = "D:\\deca_gui-b595\\work\\hp"
+PROJ = DECA_PATH + "\\project.json"
+FILE = "extracted\\settings\\hp_settings\\animal_interest.bin"
+
+logger = logging.getLogger(__name__)
+vfs = VfsDatabase(PROJ, DECA_PATH, logger)
+fnm = FieldNameMap(vfs)
+root, data = open_file(Path(DECA_PATH + "\\" + FILE))
+
+hashes = {}
+for node in root.child_table:
+    hashes[node.name_hash] = fnm.lookup(hash32=node.name_hash)
+
+with open('animal_interest.json', 'w') as f:
+    json.dump({"equipment_name_hash": hashes}, f, indent=4)
